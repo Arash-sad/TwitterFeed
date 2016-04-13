5cc50027c5a7fd0a1c173db015f374572bdbdf76
DrArash <arash_part2@yahoo.com>
2016-04-13 18:03:26 +1000

    Initial Commit

---
+++ SampleTwitterFeedApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleTwitterFeedApp
+//
+//  Created by Arash Sadeghieh E on 13/04/2016.
+//  Copyright © 2016 Arash Sadeghieh Eshtehadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
